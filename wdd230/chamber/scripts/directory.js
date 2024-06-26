a08ac27480637915d353e86047d6f1114d4a8e65
Makai-Maetani <makai@byui.edu>
2024-06-25 20:29:03 -0600

    asdf

--- wdd230/chamber/scripts/directory.js
+++ wdd230/chamber/scripts/directory.js
@@ -32,7 +32,6 @@ fetch(dataURL)
         
         website.setAttribute("href", business.website);
         image.setAttribute("src", business.images);
-        console.log(business.images);
         image.setAttribute("alt", `Image of ${business.name}`);
         image.setAttribute("loading", "lazy");
        
