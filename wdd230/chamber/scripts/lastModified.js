83490bfc0835ecb2147f49f50b49f469feaa92e9
Makai-Maetani <makai@byui.edu>
2024-06-30 19:49:58 -0600

    adsf

--- wdd230/chamber/scripts/lastModified.js
+++ wdd230/chamber/scripts/lastModified.js
@@ -4,12 +4,3 @@ let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-dig
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
 
-window.addEventListener("load", (event) => {
-    const now = new Date();
-    const day = now.getDay(); 
-    // 2 = monday, 4 = widnesday
-    // if (day >= 2 && day <= 4){
-    if (day == 0){
-      document.querySelector("#post-editor-78687221").style.display = "block"
-     }
-    });
