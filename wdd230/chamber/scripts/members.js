f5507455be1d04604fbbe2d2e4e5c019bfcda56a
Makai-Maetani <makai@byui.edu>
2024-07-07 23:20:44 -0600

    asdf

---
+++ wdd230/chamber/scripts/members.js
@@ -0,0 +1,13 @@
+fetch('https://makai-maetani.github.io//wdd230/chamber/data/members.json')
+.then(function (response){
+    return response.json();
+})
+.then(function(data){
+    for(var i=0; i<data.length; i++){
+        document.getElementById("data").innerHTML +=
+        data[i].id + "=>" + data[i].level + " " + data[i].name + "<br />" 
+    }
+})
+.catch(function (err){
+    console.log(err);
+});
